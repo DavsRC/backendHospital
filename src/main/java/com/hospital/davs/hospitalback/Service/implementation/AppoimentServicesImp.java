0223049926014368e536c83846a17bf5042d9ce9
Davinson Rincon <davinson.rincon@est.iudigital.edu.co>
2022-07-30 18:37:38 -0500

    Appoiments Services Implementation

--- src/main/java/com/hospital/davs/hospitalback/Service/implementation/AppoimentServicesImp.java
+++ src/main/java/com/hospital/davs/hospitalback/Service/implementation/AppoimentServicesImp.java
@@ -1,6 +1,33 @@
 package com.hospital.davs.hospitalback.Service.implementation;
 
 import com.hospital.davs.hospitalback.Service.AppoimentServices;
+import com.hospital.davs.hospitalback.model.AppoimentDTO;
+
+import java.util.Collection;
 
 public class AppoimentServicesImp implements AppoimentServices {
+    @Override
+    public AppoimentDTO create(AppoimentDTO appomentDTO) {
+        return null;
+    }
+
+    @Override
+    public Collection<AppoimentDTO> list(int limit) {
+        return null;
+    }
+
+    @Override
+    public AppoimentDTO get(Long id) {
+        return null;
+    }
+
+    @Override
+    public AppoimentDTO update(Long id) {
+        return null;
+    }
+
+    @Override
+    public Boolean delete(Long id) {
+        return null;
+    }
 }
