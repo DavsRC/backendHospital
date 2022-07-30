370353603d1f570205347115334d92c722a470ff
Davinson Rincon <davinson.rincon@est.iudigital.edu.co>
2022-07-30 06:53:52 -0500

    Sping Initializr Structure

---
+++ src/main/java/com/hospital/davs/hospitalback/HospitalbackApplication.java
@@ -0,0 +1,13 @@
+package com.hospital.davs.hospitalback;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HospitalbackApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HospitalbackApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/hospital/davs/hospitalback/HospitalbackApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hospital.davs.hospitalback;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HospitalbackApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
